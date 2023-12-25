8595899131ad29819d27ccfc703140576cb2388c
Krishal Shah <krishals.001@gmail.com>
2023-12-25 12:00:27 +0530

    bug/fixed background color mismatch

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -56,7 +56,7 @@ const DocumentIdPage = ({ params: { documentId } }: DocumentIdPageProps) => {
   }
 
   return (
-    <div className='pb-40'>
+    <div className='pb-40 dark:bg-[#1F1F1F]'>
       <Cover url={document.coverImage} />
       <div className='md:max-w-3xl lg:max-w-4xl mx-auto'>
         <Toolbar initialData={document} />
